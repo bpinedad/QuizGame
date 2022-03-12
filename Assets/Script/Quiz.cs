4073957b1dbf15fac1c60956a2b2dc03650848f6
Brandon Pineda <bpinedad96@gmail.com>
2022-03-11 23:11:36 -0600

    Added Scriptable objects and GUI elements
    Added Question layout
    Added Answers layout
    Added scriptable objects with question, answers and correct answer

---
+++ Assets/Script/Quiz.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] QuestionSO question;
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] GameObject[] answerButtons;
+
+    void Start()
+    {
+        questionText.text = question.GetQuestion();
+
+        for (int i= 0; i<answerButtons.Length ; i++) {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+}
